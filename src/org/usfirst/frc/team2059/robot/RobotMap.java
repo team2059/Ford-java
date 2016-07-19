9f6eeacb8ef1e5a04af8c50c6738ee016a33cba1
Adam Long <macadam.long@gmail.com>
2016-07-19 17:48:31 +0000

    added values to robotmap

--- src/org/usfirst/frc/team2059/robot/RobotMap.java
+++ src/org/usfirst/frc/team2059/robot/RobotMap.java
@@ -1,18 +1,37 @@
 package org.usfirst.frc.team2059.robot;
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+
+   //Drive
+   public static int driveLeftMotorOne = 1;
+   public static int driveLeftMotorTwo = 2;
+   public static int driveRightMotorOne = 1;
+   public static int driveRightMotorTwo = 2;
+   public static int driveRightEncoder = 0;
+   public static int driveLeftEncoder = 1;
+
+   //Arm
+   public static double zeroDegrees = 0.1;
+   public static double ninetyDegrees = 0.7;
+   public static int armPot = 0;
+   public static int armLeftMotor = 5;
+   public static int armRightMotor = 6;
+
+   //Shooter
+   public static int shooterLeftMotor = 7;
+   public static int shooterRightMotor = 8;
+
+   //Pneumatics
+   public static int pcmID = 31;
+   public static int shooterSolenoidOne = 0;
+   public static int shooterSolenoidTwo = 1;
+   public static int portcullisSolenoidOne = 2;
+   public static int portcullisSolenoidTwo = 3;
+   public static int armStopSolenoidOne = 4;
+   public static int armStopSolenoidTwo = 5;
+
+   //Misc
+   public static int mainArmPresetCollect = 0;
+   public static int mainArmPresetTraverse = 5;
+   public static int mainArmPresetCloseShot = 95;
+   public static int mainArmPresetFarShot = 85;
 }
