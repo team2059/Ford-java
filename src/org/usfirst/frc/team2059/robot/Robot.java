69e7cddd452b7913acf0f45f5c74dea334bd731b
Austen Adler <stonewareslord@gmail.com>
2016-07-20 15:33:32 -0400

    Remove ExampleCommand and ExampleSubsystem

--- src/org/usfirst/frc/team2059/robot/Robot.java
+++ src/org/usfirst/frc/team2059/robot/Robot.java
@@ -3,8 +3,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team2059.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2059.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
@@ -15,7 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-  public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
   public static OI oi;
   Command autonomousCommand;
   SendableChooser chooser;
@@ -26,7 +23,6 @@ public class Robot extends IterativeRobot {
   public void robotInit() {
     oi = new OI();
     chooser = new SendableChooser();
-    chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", chooser);
   }
